b49852971711f7d678af977c17ee953b5e6bc16a
fastboy <giagiang200@gmail.com>
2023-10-13 13:39:23 +0700

    create suggestions

--- src/timeline/Sugesstions.js
+++ src/timeline/Sugesstions.js
@@ -1,10 +1,81 @@
-import React from 'react';
-import './Sugesstions.css';
+import React from "react";
+import { Avatar } from "@mui/material";
+import "./Sugesstions.css";
 
 function Sugesstions() {
   return (
-    <div>Sugesstions</div>
-  )
+    <div className="suggestions">
+      <div className="suggestions__title"> Suggested for you</div>
+      <div className="suggestions__usernames">
+        <div className="suggestions__username">
+          <div className="username__left">
+            <span className="avatar">
+              <Avatar>r</Avatar>
+            </span>
+            <div className="username_info">
+              <span className="username">giang</span>
+              <span className="relation"> follow by ha.leps</span>
+            </div>
+          </div>
+          <button className="follow__button">Follow </button>
+        </div>
+        
+        <div className="suggestions__username">
+          <div className="username__left">
+            <span className="avatar">
+              <Avatar>r</Avatar>
+            </span>
+            <div className="username_info">
+              <span className="username">giang</span>
+              <span className="relation"> follow by ha.leps</span>
+            </div>
+          </div>
+          <button className="follow__button">Follow </button>
+        </div>
+
+        <div className="suggestions__username">
+          <div className="username__left">
+            <span className="avatar">
+              <Avatar>r</Avatar>
+            </span>
+            <div className="username_info">
+              <span className="username">giang</span>
+              <span className="relation"> follow by ha.leps</span>
+            </div>
+          </div>
+          <button className="follow__button">Follow </button>
+        </div>
+
+        <div className="suggestions__username">
+          <div className="username__left">
+            <span className="avatar">
+              <Avatar>r</Avatar>
+            </span>
+            <div className="username_info">
+              <span className="username">giang</span>
+              <span className="relation"> follow by ha.leps</span>
+            </div>
+          </div>
+          <button className="follow__button">Follow </button>
+        </div>
+        
+
+        <div className="suggestions__username">
+          <div className="username__left">
+            <span className="avatar">
+              <Avatar>r</Avatar>
+            </span>
+            <div className="username_info">
+              <span className="username">giang</span>
+              <span className="relation"> follow by ha.leps</span>
+            </div>
+          </div>
+          <button className="follow__button">Follow </button>
+        </div>
+
+      </div>
+    </div>
+  );
 }
 
-export default Sugesstions
+export default Sugesstions;
